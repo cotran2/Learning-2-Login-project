8b533d6c4f270f5461c81b515e817305449c13e4
gapower0 <gapower0@gmail.com>
2017-04-13 22:25:51 -0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Account
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection con = new MySqlConnection("server=localhost;user id=qtran;password=19031998;persistsecurityinfo=True;database=accountdatabase");
+        int i;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            i = 0;
+            con.Open();
+            MySqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText ="select *from login where Username='"+textBox1.Text+"'and password='"+textBox2.Text+"'";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+            da.Fill(dt);
+            i = Convert.ToInt32(dt.Rows.Count.ToString());
+            if (i == 0)
+            {
+                MessageBox.Show("You entered invalid username or password");
+            }
+            else
+            {
+                this.Hide();
+                Form2 fm = new Form2();
+                fm.Show();
+            }
+
+            con.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form3 fm3 = new Form3();
+            fm3.Show();
+        }
+    }
+}
---
+++ Form3.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Account
+{
+    public partial class Form3 : Form
+    {
+        MySqlConnection con1 = new MySqlConnection("server=localhost;user id=qtran;password=19031998;persistsecurityinfo=True;database=accountdatabase");
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlCommand cmd1 = new MySqlCommand("insert into login(ID,UserName,Password)Values('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "')", con1);
+            con1.Open();
+            cmd1.ExecuteNonQuery();
+            MessageBox.Show("Save Success!");
+            con1.Close();
+            this.Hide();
+            Form1 fm1 = new Form1();
+            fm1.Show();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
